41bb3f78e7a582f8c2e68a3c529fe465ba14867a
Reggiezu <reginaldosakue@gmail.com>
2026-03-03 16:01:46 -0500

    Feat: Added rendering

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-// ToDo List //
+// ****ToDo List ****
 
-// List state functions 
+// ***List state functions*** 
 const toDoList = (function(){
 let projects=["default","Java"]
 let itemList=[{
@@ -11,13 +11,21 @@ let itemList=[{
      priority:3,
      project:"default",
      complete:false
-}];
+},{
+    id:25,
+    title:"test",
+     description:"Here to test",
+     dueDate:"02-20-2026",
+     priority:3,
+     project:"default",
+     complete:false
+},];
 function newItem (title,
      description,
      dueDate,
      priority,
      project){
-    const id = crypto.randomUUID();
+    const id ="TID-" +crypto.randomUUID();
     const complete = false;
     title = titleCleaner(title);
     if (!priorityChecker(priority)){
@@ -90,8 +98,36 @@ return{
 };
 
 })();
+//****Rendering pieces ***
+const taskList = document.getElementById("Item-List")
+const taskCard = document.getElementById("Task-Card")
+const taskTitle = document.getElementById("Task-Title")
+const taskDescription = document.getElementById("Task-Description")
+const taskDueDate = document.getElementById("Task-DueDate")
+const taskPriority = document.getElementById("Task-Priority")
+const taskProject = document.getElementById("Task-Project")
+const taskCompletion = document.getElementById("Task-Completion")
+const delBtn = document.querySelectorAll("button")
+
+console.log("This is the first:" + taskList)
+
+function renderList() {
+    const list = toDoList.getList();
+     list.map((task) => taskList.innerHTML +=`<div id="Task-Card-${task.id}" class="card">
+            <span id="Task-Title-${task.id}" class="cardItem"><strong>Title: </strong> ${task.title} </span>
+            <p id="Task-Description-${task.id}" class="cardItem"><strong>Description: </strong> ${task.description}</p>
+            <p id="Task-DueDate-${task.id}" class="cardItem"><strong>Due Date: </strong> ${task.dueDate} </p>
+            <p id="Task-Priority-${task.id}" class="cardItem"><strong>Priority: </strong> ${task.priority}</p>
+            <p id="Task-Project-${task.id}" class="cardItem"><strong>Project: </strong> ${task.project}</p>
+            <p id="Task-Completion-${task.id}" class="cardItem"><strong> Completed: </strong >${task.complete} </p>
+            <button id="Task-Deletion-${task.id}" > Delete Task</button>
+        </div>`)
+}
+
+
 
 toDoList.newItem("  Cook.     .","Grill ribs and start rice cooker",'08-22-2024', 4)
 toDoList.newItem("Eat","Set Plate and chow down",'08-22-2024', 5, "Habits")
-toDoList.newItem("Eat","Set Plate and chow down",'08-22-2024', 90, "Habits")
+toDoList.newItem("testingError","My priority is wrong",'08-22-2024', 90, "Habits")
 
+renderList();
